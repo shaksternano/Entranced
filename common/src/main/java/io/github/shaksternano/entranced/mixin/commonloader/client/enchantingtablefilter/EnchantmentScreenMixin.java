610999a869f6181836c905b96215274687283975
Shukai Zhang <54268387+ShakterNano@users.noreply.github.com>
2022-04-05 12:52:56 +0100

    Make the panel hidden if turned off in config

--- common/src/main/java/io/github/shaksternano/entranced/mixin/commonloader/client/enchantingtablefilter/EnchantmentScreenMixin.java
+++ common/src/main/java/io/github/shaksternano/entranced/mixin/commonloader/client/enchantingtablefilter/EnchantmentScreenMixin.java
@@ -1,6 +1,7 @@
 package io.github.shaksternano.entranced.mixin.commonloader.client.enchantingtablefilter;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import io.github.shaksternano.entranced.commonside.Entranced;
 import io.github.shaksternano.entranced.commonside.gui.EnchantingCatalystPanel;
 import io.github.shaksternano.entranced.mixin.commonloader.client.accessor.HandledScreenAccessor;
 import net.fabricmc.api.EnvType;
@@ -25,40 +26,44 @@ abstract class EnchantmentScreenMixin extends HandledScreenMixin {
 
     @Inject(method = "drawBackground", at = @At("RETURN"))
     private void entranced$addEnchantingCatalystGui(MatrixStack matrices, float delta, int mouseX, int mouseY, CallbackInfo ci) {
-        RenderSystem.setShaderTexture(0, EnchantingCatalystPanel.INSTANCE.getGuiTexture());
+        if (Entranced.getConfig().isEnchantingCatalystEnabled()) {
+            RenderSystem.setShaderTexture(0, EnchantingCatalystPanel.INSTANCE.getGuiTexture());
 
         /*
         Need to use this instead of using a @Shadow on the x and y fields to avoid a NoSuchFieldError on Forge.
          */
-        HandledScreenAccessor handledScreenAccessor = (HandledScreenAccessor) this;
-        int x = handledScreenAccessor.entranced$getX();
-        int y = handledScreenAccessor.entranced$getY();
+            HandledScreenAccessor handledScreenAccessor = (HandledScreenAccessor) this;
+            int x = handledScreenAccessor.entranced$getX();
+            int y = handledScreenAccessor.entranced$getY();
 
-        drawTexture(
-                matrices,
-                EnchantingCatalystPanel.INSTANCE.getTextureX(x),
-                EnchantingCatalystPanel.INSTANCE.getTextureY(y),
-                0,
-                0,
-                EnchantingCatalystPanel.INSTANCE.getGuiTextureWidth(),
-                EnchantingCatalystPanel.INSTANCE.getGuiTextureHeight()
-        );
+            drawTexture(
+                    matrices,
+                    EnchantingCatalystPanel.INSTANCE.getTextureX(x),
+                    EnchantingCatalystPanel.INSTANCE.getTextureY(y),
+                    0,
+                    0,
+                    EnchantingCatalystPanel.INSTANCE.getGuiTextureWidth(),
+                    EnchantingCatalystPanel.INSTANCE.getGuiTextureHeight()
+            );
+        }
     }
 
     @Override
     protected void entranced$expandBounds(double mouseX, double mouseY, int left, int top, int button, CallbackInfoReturnable<Boolean> cir) {
-        /*
-        Need to use this instead of using a @Shadow on the x and y fields to avoid a NoSuchFieldError on Forge.
-         */
-        HandledScreenAccessor handledScreenAccessor = (HandledScreenAccessor) this;
-        int x = handledScreenAccessor.entranced$getX();
-        int y = handledScreenAccessor.entranced$getY();
+        if (Entranced.getConfig().isEnchantingCatalystEnabled()) {
+            /*
+            Need to use this instead of using a @Shadow on the x and y fields to avoid a NoSuchFieldError on Forge.
+             */
+            HandledScreenAccessor handledScreenAccessor = (HandledScreenAccessor) this;
+            int x = handledScreenAccessor.entranced$getX();
+            int y = handledScreenAccessor.entranced$getY();
 
-        if (mouseX >= EnchantingCatalystPanel.INSTANCE.getTextureX(x)) {
-            if (mouseX <= EnchantingCatalystPanel.INSTANCE.getTextureX(x) + EnchantingCatalystPanel.INSTANCE.getGuiTextureWidth()) {
-                if (mouseY >= EnchantingCatalystPanel.INSTANCE.getTextureY(y)) {
-                    if (mouseY <= EnchantingCatalystPanel.INSTANCE.getTextureY(y) + EnchantingCatalystPanel.INSTANCE.getGuiTextureHeight()) {
-                        cir.setReturnValue(false);
+            if (mouseX >= EnchantingCatalystPanel.INSTANCE.getTextureX(x)) {
+                if (mouseX <= EnchantingCatalystPanel.INSTANCE.getTextureX(x) + EnchantingCatalystPanel.INSTANCE.getGuiTextureWidth()) {
+                    if (mouseY >= EnchantingCatalystPanel.INSTANCE.getTextureY(y)) {
+                        if (mouseY <= EnchantingCatalystPanel.INSTANCE.getTextureY(y) + EnchantingCatalystPanel.INSTANCE.getGuiTextureHeight()) {
+                            cir.setReturnValue(false);
+                        }
                     }
                 }
             }
